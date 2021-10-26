1b1b60a553e871e6f5698861249cf7a40a136967
kitoshi <kitoshi.charlton@gmail.com>
2021-10-26 10:35:49 -0700

    HTML skeleton and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Carousel from "./components/Carousel.jsx";
+import Instagram from "./components/Instagram.jsx";
+import penny from "./images/pliu2.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>P.Lieu</h1>
+      <header className="header">
+        <h1>Penny Liu</h1>
+        <img
+          src={penny}
+          alt="Penny"
+        ></img>
       </header>
+      <Instagram />
       <Carousel />
     </div>
   );
---
+++ src/components/Instagram.jsx
@@ -0,0 +1,15 @@
+function Instagram() {
+  return (
+    <>
+      <div>API CALL Followers</div>
+      <div>API CALL Following</div>
+      <nav>
+        <button>Gallery</button>
+        <button>About</button>
+        <button>Instagram</button>
+      </nav>
+    </>
+  );
+}
+
+export default Instagram;
