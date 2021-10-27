76a1e98c3aa0ff707c4af46955db0020214a268a
kitoshi <kitoshi.charlton@gmail.com>
2021-10-26 21:01:08 -0700

    padding fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Carousel from "./components/Carousel.jsx";
-import Instagram from "./components/Instagram.jsx";
 import penny from "./images/penny.jpg";
 import About from "./components/About";
 
@@ -9,7 +8,7 @@ function App() {
     <div className="App">
       <header className="header">
         <div>
-          <div className="insta">@pdogmomma</div>
+          <div className="insta">@pennyliu07</div>
           <h1>Penny Liu</h1>
           <img src={penny} alt="Penny"></img>
         </div>
