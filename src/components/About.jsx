a7bb77cd8273b74ab59ef2c6828ad27a94311e65
kitoshi <kitoshi.charlton@gmail.com>
2021-11-17 12:45:05 -0800

    changed anchor to js for no url change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,8 @@
 function About() {
+  function goFooter() {
+    const footer = document.getElementById("footer");
+    footer.scrollIntoView();
+  }
   return (
     <div className="aboutcontainer">
       <h2>Vancouver, BC</h2>
@@ -8,7 +12,7 @@ function About() {
       <p className="about">Each piece is an expression of my life.</p>
       <p className="about">
         <strong>
-          <a href="#footer">Let me express your life.</a>
+          <u onClick={goFooter}>Let me express your life.</u>
         </strong>
       </p>
     </div>
